824596ada9c5ffdbaec1074f15eeac6b78a6762c
Amit Ben Arie <amit.benarie@post.runi.ac.il>
2024-07-19 09:58:36 +0300

    return authenticated user details

--- dummy_api_plugin.py
+++ dummy_api_plugin.py
@@ -9,7 +9,7 @@ class DummyApiPlugin(Plugin):
         self.password = password
         self.token = None
 
-    def connectivity(self):
+    def connectivity_test(self):
         endpoint = f"{self.url}/auth/login"
         try:
             response = requests.post(endpoint, json={'username': self.username, 'password': self.password})
@@ -24,3 +24,24 @@ class DummyApiPlugin(Plugin):
         except requests.RequestException as e:
             print(f"error: {e}")
             return False
+
+    def authenticated_user(self):
+        # check if authentication was done
+        if not self.token:
+            return None
+
+        # provided the token get current authenticated user
+        address = f"{self.url}/user/me"
+
+        try:
+            # GET request with the Bearer token
+            response = requests.get(address, {"Authorization": f"Bearer {self.token}"})
+            if response.status_code == 200:
+                data = response.json()
+                return data
+            else:
+                print("Failed attempt:", response.status_code)
+                return None
+        except requests.RequestException as e:
+            print(f"Error: {e}")
+            return None
